f329ca7e7a3999edb0ea9c2aa493fe89caaa3265
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 18:31:33 +0700

    Weather Observation Station 6.sql

---
+++ Basic_Select/Weather Observation Station 6.sql
@@ -0,0 +1,28 @@
+/*
+QUESTION:
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+The STATION table is described as follows:
+- ID : NUMBER
+- CITY : VARCHAR(21)
+- STATE : VARCHAR(2)
+- LAT_N : NUMBER
+- LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+ANSWER : 
+-- Using MySQL
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' 
+    OR CITY LIKE 'I%'
+    OR CITY LIKE 'U%'
+    OR CITY LIKE 'E%'
+    OR CITY LIKE 'O%'
+    OR CITY LIKE 'a%'
+    OR CITY LIKE 'i%'
+    OR CITY LIKE 'u%'
+    OR CITY LIKE 'e%'
+    OR CITY LIKE 'o%';
+    
