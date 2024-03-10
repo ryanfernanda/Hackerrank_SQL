8a93ba81fa4f889273c25d3b8368f807eae6644b
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 17:25:17 +0700

    Weather Observation Station 3.sql

---
+++ Basic_Select/Weather Observation Station 3.sql
@@ -0,0 +1,19 @@
+/*
+QUESTION:
+Query a list of CITY names from STATION for cities that have an even ID number. 
+Print the results in any order, but exclude duplicates from the answer.
+The STATION table is described as follows:
+- ID : NUMBER
+- CITY : VARCHAR(21)
+- STATE : VARCHAR(2)
+- LAT_N : NUMBER
+- LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+ANSWER : 
+-- Using MySQL
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID%2 = 0;
