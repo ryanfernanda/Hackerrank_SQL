1d8d74c615daa07fcd815ccf8bf1c9dbb9cc1cf4
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 17:04:58 +0700

    Create Select All.sql

---
+++ Basic_Select/Select All.sql
@@ -0,0 +1,16 @@
+/*
+QUESTION:
+Query all columns (attributes) for every row in the CITY table.
+The CITY table is described as follows:
+- ID : NUMBER
+- NAME : VARCHAR(17)
+- COUNTRYCODE : VARCHAR(3)
+- DISTRICT : VARCHAR(20)
+- POPULATION : NUMBER
+
+ANSWER : 
+* Using MySQL
+*/
+
+SELECT *
+FROM CITY;
