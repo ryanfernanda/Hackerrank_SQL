9d02215ebba17e16de5ceb66f5e78d7827c42432
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 19:05:16 +0700

    Weather Observation Station 10.sql

---
+++ Basic_Select/Weather Observation Station 10.sql
@@ -0,0 +1,24 @@
+/*
+QUESTION:
+Query the list of CITY names from STATION that do not ends with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+- ID : NUMBER
+- CITY : VARCHAR(21)
+- STATE : VARCHAR(2)
+- LAT_N : NUMBER
+- LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+ANSWER : 
+-- Using MySQL
+*/
+
+SELECT  DISTINCT CITY
+FROM STATION
+WHERE SUBSTRING(CITY,-1,1) NOT IN ('A','E','I','O','U','a','e','i','u','o');
+
+-- We can also use the Regular Expression for the shorter query :
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP "[^AEIUOaeiuo]$";
