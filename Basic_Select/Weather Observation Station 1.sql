d9b7b0c6028fe7c1b888832fc6a4134c9a739f13
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 17:23:48 +0700

    Weather Observation Station 1.sql

--- Basic_Select/Weather Observation Station 1.sql
+++ Basic_Select/Weather Observation Station 1.sql
@@ -7,6 +7,7 @@ The STATION table is described as follows:
 - STATE : VARCHAR(2)
 - LAT_N : NUMBER
 - LONG_W : NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 ANSWER : 
 -- Using MySQL
