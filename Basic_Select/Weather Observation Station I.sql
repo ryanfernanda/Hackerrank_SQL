69af32f58f0cca9bf892f1bdb8fa173be19e2322
Ryan Fernanda <84463811+ryanfernanda@users.noreply.github.com>
2024-03-10 17:18:12 +0700

    Weather Observation Station I.sql

---
+++ Basic_Select/Weather Observation Station I.sql
@@ -0,0 +1,16 @@
+/*
+QUESTION:
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+- ID : NUMBER
+- CITY : VARCHAR(21)
+- STATE : VARCHAR(2)
+- LAT_N : NUMBER
+- LONG_W : NUMBER
+
+ANSWER : 
+-- Using MySQL
+*/
+
+SELECT CITY, STATE
+FROM STATION;
